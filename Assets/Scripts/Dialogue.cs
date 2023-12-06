c0a563f91faa9f63b800f657e11cd3297402571f
OgnestrelYT <arssamohvalov@gmail.com>
2023-12-07 01:00:35 +0300

    1.5 full worked dialogs

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,12 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[System.Serializable]
+using UnityEngine.UI;
 
 public class Dialogue : MonoBehaviour
 {
-    public string name;
-    [TextArea(3, 10)]
-    public string[] sentences;
+    public GameObject winDialog;
+    public bool stopted = false;
+    public bool repeatAfterLeaving = true;
+    public Text text;
+    public Text nameText;
+    public Button button;
+    public Image avatarObj;
+    public Sprite avatarImg;
+
+    public string nameOfCharacter;
+
+    public string[] message;
+    public int numDialog = 0;
+
+    private void Start()
+    {
+        winDialog.SetActive(false);
+        nameText.text = nameOfCharacter;
+        avatarObj.sprite = avatarImg;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (numDialog + 2 > message.Length)
+            {
+                if (repeatAfterLeaving)
+                {
+                    numDialog = 0;
+                    button.onClick.AddListener(NextDialog);
+                    text.text = message[numDialog];
+                    winDialog.SetActive(true);
+                } else {
+                    winDialog.SetActive(false);
+                    button.onClick.RemoveAllListeners();
+                }
+            }
+            else
+            {
+                button.onClick.AddListener(NextDialog);
+                text.text = message[numDialog];
+                winDialog.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            winDialog.SetActive(false);
+            button.onClick.RemoveAllListeners();
+        }
+    }
+
+    public void NextDialog()
+    {
+        if (numDialog + 2 > message.Length)
+        {
+            winDialog.SetActive(false);
+            button.onClick.RemoveAllListeners();
+        }
+
+        if (!stopted)
+        {
+            numDialog++;
+        }
+        text.text = message[numDialog];
+    }
 }
